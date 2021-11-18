26174a0806f6a2d5716971cdbf02a2284ba16ec6
Adaku Inem <80346851+nenayah@users.noreply.github.com>
2021-11-18 10:57:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Code to perform web scraping for weather data of current region
-# using requests-html
+# using requests-html library
 from bs4 import *
 from requests_html import HTMLSession
-
+import requests
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 # US english
 LANGUAGE = "en-US,en;q=0.5"
@@ -59,6 +59,18 @@ def get_weather_data(url):
 # finish up the script by parsing command-line arguments
 if __name__ == "__main__":
     URL = "https://www.google.com/search?q=weather&sxsrf=AOaemvKHG3CukxUHhL7rjD8MTxxnyKGr7g%3A1634891839837&ei=P3hyYYfFMoq5gweSya-4DA&ved=0ahUKEwjH4O_azt3zAhWK3OAKHZLkC8cQ4dUDCA4&uact=5&oq=weather&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGABQ-ugZWProGWCN6xloAHACeACAAQCIAQCSAQCYAQCgAQHAAQE&sclient=gws-wiz"
+
+    #To check for internet connection
+    timeout = 5
+    try:
+        request = requests.get(URL, timeout=timeout)
+        Internet = True
+        print("Connected to the Internet")
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        Internet = False
+        print("No internet connection.")
+
+    #To retrieve weather data
     import argparse
 
     parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
@@ -71,58 +83,59 @@ if __name__ == "__main__":
     # get data
     data = get_weather_data(URL)
 
-    print("Weather for:", data["region"])
-    print("Now:", data["dayhour"])
-    print(f"Temperature now: {data['temp_now']}°C")
-    print("Description:", data['weather_now'])
-    print("Precipitation:", data["precipitation"])
-    print("Humidity:", data["humidity"])
-    print("Wind:", data["wind"])
+    if Internet is True:
+        print("Weather for:", data["region"])
+        print("Now:", data["dayhour"])
+        print(f"Temperature now: {data['temp_now']}°C")
+        print("Description:", data['weather_now'])
+        print("Precipitation:", data["precipitation"])
+        print("Humidity:", data["humidity"])
+        print("Wind:", data["wind"])
 
-    print(" ")
-    print("Each day for the next week:")
+        print(" ")
+        print("Each day for the next week:")
 
-    if "Monday" in data["dayhour"]:
+        if "Monday" in data["dayhour"]:
+            i = 0
+        elif "Tuesday" in data["dayhour"]:
+            i = 1
+        elif "Wednesday" in data["dayhour"]:
+            i = 2
+        elif "Thursday" in data["dayhour"]:
+            i = 3
+        elif "Friday" in data["dayhour"]:
+            i = 4
+        elif "Saturday" in data["dayhour"]:
+            i = 5
+        elif "Sunday" in data["dayhour"]:
+            i = 6
+
+        week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        safe = i
+        while i < 7:
+            next_url = "https://www.google.com/search?q=" + week[i] + "+weather&sxsrf=AOaemvKHG3CukxUHhL7rjD8MTxxnyKGr7g%3A1634891839837&ei=P3hyYYfFMoq5gweSya-4DA&ved=0ahUKEwjH4O_azt3zAhWK3OAKHZLkC8cQ4dUDCA4&uact=5&oq=weather&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGABQ-ugZWProGWCN6xloAHACeACAAQCIAQCSAQCYAQCgAQHAAQE&sclient=gws-wiz"
+            next_data = get_weather_data(next_url)
+            weather_next_dict = {
+                "region": next_data["region"],
+                "day_hour": next_data["dayhour"],
+                "weather_description": next_data['weather_now'],
+                "precipitation": next_data["precipitation"],
+                "humidity": next_data["humidity"],
+                "wind": next_data["wind"]
+            }
+            print(weather_next_dict)
+            i += 1
         i = 0
-    elif "Tuesday" in data["dayhour"]:
-        i = 1
-    elif "Wednesday" in data["dayhour"]:
-        i = 2
-    elif "Thursday" in data["dayhour"]:
-        i = 3
-    elif "Friday" in data["dayhour"]:
-        i = 4
-    elif "Saturday" in data["dayhour"]:
-        i = 5
-    elif "Sunday" in data["dayhour"]:
-        i = 6
-
-    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    safe = i
-    while i < 7:
-        next_url = "https://www.google.com/search?q=" + week[i] + "+weather&sxsrf=AOaemvKHG3CukxUHhL7rjD8MTxxnyKGr7g%3A1634891839837&ei=P3hyYYfFMoq5gweSya-4DA&ved=0ahUKEwjH4O_azt3zAhWK3OAKHZLkC8cQ4dUDCA4&uact=5&oq=weather&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGABQ-ugZWProGWCN6xloAHACeACAAQCIAQCSAQCYAQCgAQHAAQE&sclient=gws-wiz"
-        next_data = get_weather_data(next_url)
-        weather_next_dict = {
-            "region": next_data["region"],
-            "day_hour": next_data["dayhour"],
-            "weather_description": next_data['weather_now'],
-            "precipitation": next_data["precipitation"],
-            "humidity": next_data["humidity"],
-            "wind": next_data["wind"]
-        }
-        print(weather_next_dict)
-        i += 1
-    i = 0
-    while i < safe:
-        next_url = "https://www.google.com/search?q=" + week[i] + "+weather&sxsrf=AOaemvKHG3CukxUHhL7rjD8MTxxnyKGr7g%3A1634891839837&ei=P3hyYYfFMoq5gweSya-4DA&ved=0ahUKEwjH4O_azt3zAhWK3OAKHZLkC8cQ4dUDCA4&uact=5&oq=weather&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGABQ-ugZWProGWCN6xloAHACeACAAQCIAQCSAQCYAQCgAQHAAQE&sclient=gws-wiz"
-        next_data = get_weather_data(next_url)
-        weather_next_dict = {
-            "region": next_data["region"],
-            "day_hour": next_data["dayhour"],
-            "weather_description": next_data['weather_now'],
-            "precipitation": next_data["precipitation"],
-            "humidity": next_data["humidity"],
-            "wind": next_data["wind"]
-        }
-        print(weather_next_dict)
-        i += 1
+        while i < safe:
+            next_url = "https://www.google.com/search?q=" + week[i] + "+weather&sxsrf=AOaemvKHG3CukxUHhL7rjD8MTxxnyKGr7g%3A1634891839837&ei=P3hyYYfFMoq5gweSya-4DA&ved=0ahUKEwjH4O_azt3zAhWK3OAKHZLkC8cQ4dUDCA4&uact=5&oq=weather&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGABQ-ugZWProGWCN6xloAHACeACAAQCIAQCSAQCYAQCgAQHAAQE&sclient=gws-wiz"
+            next_data = get_weather_data(next_url)
+            weather_next_dict = {
+                "region": next_data["region"],
+                "day_hour": next_data["dayhour"],
+                "weather_description": next_data['weather_now'],
+                "precipitation": next_data["precipitation"],
+                "humidity": next_data["humidity"],
+                "wind": next_data["wind"]
+            }
+            print(weather_next_dict)
+            i += 1
